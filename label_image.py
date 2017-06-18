92745fa5d2e0dcf9759822ba371b8b2e0aaa44ec
Ben Muschol <benmuschol@gmail.com>
2017-06-18 15:21:27 -0400

    Add label image functionality

---
+++ label_image.py
@@ -0,0 +1,30 @@
+import tensorflow as tf, sys
+
+# constants
+LABELS_FILE = './output_labels.txt'
+GRAPH_FILE = './output_graph.pb'
+
+# load image
+image_path = sys.argv[1]
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+# parse labels file
+label_lines = [ line.rstrip() for line in tf.gfile.GFile(LABELS_FILE) ]
+
+# load the trained model from the output graph
+with tf.gfile.FastGFile(GRAPH_FILE, 'rb') as f:
+    graph = tf.GraphDef()
+    graph.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph, name='')
+
+with tf.Session() as sess:
+    # pass the image into the graph as input
+    tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+    predictions = sess.run(tensor, { 'DecodeJpeg/contents:0': image_data })
+    sorted_predictions = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+    for node in sorted_predictions:
+        label = label_lines[node]
+        score = predictions[0][node]
+        print "%s: %.5f" % (label, score)
